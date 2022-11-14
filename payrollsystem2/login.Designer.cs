7eff5faccf1f8d9a5c00144db527d82813722b16
Administrator <Administrator@DESKTOP-DPN0L94>
2022-11-14 13:20:39 +0800

    Add project files.

---
+++ payrollsystem2/login.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace payrollsystem2
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lbluser = new System.Windows.Forms.Label();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.lblpass = new System.Windows.Forms.Label();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.comboxuser = new System.Windows.Forms.ComboBox();
+            this.lblselectposition = new System.Windows.Forms.Label();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel3.Controls.Add(this.btnlogin);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Controls.Add(this.panel2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 318);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(530, 386);
+            this.panel3.TabIndex = 3;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.BackColor = System.Drawing.SystemColors.Window;
+            this.btnlogin.FlatAppearance.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnlogin.FlatAppearance.BorderSize = 3;
+            this.btnlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlogin.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlogin.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnlogin.Location = new System.Drawing.Point(154, 295);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(214, 69);
+            this.btnlogin.TabIndex = 5;
+            this.btnlogin.Text = "LOG IN";
+            this.btnlogin.UseVisualStyleBackColor = false;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.lbluser);
+            this.panel1.Controls.Add(this.txtuser);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(17, 66);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(497, 71);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel4.Location = new System.Drawing.Point(70, 58);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(401, 10);
+            this.panel4.TabIndex = 8;
+            // 
+            // lbluser
+            // 
+            this.lbluser.AutoSize = true;
+            this.lbluser.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbluser.ForeColor = System.Drawing.SystemColors.Window;
+            this.lbluser.Location = new System.Drawing.Point(64, 18);
+            this.lbluser.Name = "lbluser";
+            this.lbluser.Size = new System.Drawing.Size(149, 34);
+            this.lbluser.TabIndex = 5;
+            this.lbluser.Text = "Username";
+            this.lbluser.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lbluser_MouseClick);
+            // 
+            // txtuser
+            // 
+            this.txtuser.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txtuser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtuser.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtuser.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtuser.Location = new System.Drawing.Point(70, 22);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(401, 30);
+            this.txtuser.TabIndex = 4;
+            this.txtuser.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtuser_MouseClick);
+            this.txtuser.TextChanged += new System.EventHandler(this.txtuser_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.lblpass);
+            this.panel2.Controls.Add(this.txtpass);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(17, 174);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(497, 71);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel5.Location = new System.Drawing.Point(70, 58);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(401, 10);
+            this.panel5.TabIndex = 9;
+            // 
+            // lblpass
+            // 
+            this.lblpass.AutoSize = true;
+            this.lblpass.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpass.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblpass.Location = new System.Drawing.Point(64, 16);
+            this.lblpass.Name = "lblpass";
+            this.lblpass.Size = new System.Drawing.Size(142, 34);
+            this.lblpass.TabIndex = 6;
+            this.lblpass.Text = "Password";
+            this.lblpass.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lblpassword_MouseClick);
+            // 
+            // txtpass
+            // 
+            this.txtpass.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txtpass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtpass.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpass.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtpass.Location = new System.Drawing.Point(70, 18);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(401, 30);
+            this.txtpass.TabIndex = 5;
+            this.txtpass.UseSystemPasswordChar = true;
+            this.txtpass.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtpass_MouseClick);
+            this.txtpass.TextChanged += new System.EventHandler(this.txtpass_TextChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 9);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(51, 49);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(154, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(214, 212);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // comboxuser
+            // 
+            this.comboxuser.FormattingEnabled = true;
+            this.comboxuser.Items.AddRange(new object[] {
+            "HR",
+            "ADMIN",
+            "EMPLOYEE"});
+            this.comboxuser.Location = new System.Drawing.Point(194, 230);
+            this.comboxuser.Name = "comboxuser";
+            this.comboxuser.Size = new System.Drawing.Size(144, 28);
+            this.comboxuser.TabIndex = 4;
+            this.comboxuser.SelectedIndexChanged += new System.EventHandler(this.comboxuser_SelectedIndexChanged);
+            this.comboxuser.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboxuser_KeyPress);
+            // 
+            // lblselectposition
+            // 
+            this.lblselectposition.AutoSize = true;
+            this.lblselectposition.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblselectposition.ForeColor = System.Drawing.Color.Red;
+            this.lblselectposition.Location = new System.Drawing.Point(179, 274);
+            this.lblselectposition.Name = "lblselectposition";
+            this.lblselectposition.Size = new System.Drawing.Size(171, 23);
+            this.lblselectposition.TabIndex = 6;
+            this.lblselectposition.Text = "Select a position";
+            // 
+            // btnexit
+            // 
+            this.btnexit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnexit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexit.Location = new System.Drawing.Point(494, 0);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(36, 39);
+            this.btnexit.TabIndex = 7;
+            this.btnexit.Text = "X";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(530, 704);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.lblselectposition);
+            this.Controls.Add(this.comboxuser);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ComboBox comboxuser;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbluser;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblpass;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblselectposition;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+    }
+}
+
---
+++ payrollsystem2/login.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace payrollsystem2
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox3.Location = new Point(100, 10);
+            lblselectposition.Hide();
+            
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lbluser_MouseClick(object sender, MouseEventArgs e)
+        {
+            lbluser.Hide();
+            txtuser.Focus();
+            
+
+            if ((txtuser.Focus() == true) && (String.IsNullOrEmpty(txtpass.Text) == true))
+            {
+                lblpass.Show();
+            }
+            else
+            {
+                lblpass.Hide();
+            }
+        }
+
+        private void lblpassword_MouseClick(object sender, MouseEventArgs e)
+        {
+            lblpass.Hide();
+            txtpass.Focus();
+            
+           
+            if ((txtpass.Focus() == true) && (String.IsNullOrEmpty(txtuser.Text) == true))
+            {
+                lbluser.Show();
+            }
+            else
+            {
+                lbluser.Hide();
+            }
+           
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+           
+            
+            if (String.IsNullOrEmpty(comboxuser.Text) == true)
+            {
+               
+                lblselectposition.Show();
+                comboxuser.Focus();
+            }
+
+            else
+            {
+                progressbar prog = new progressbar();
+                prog.Show();
+                this.Hide();
+            }
+        }
+
+        private void comboxuser_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblselectposition.Hide();
+        }
+
+        private void comboxuser_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtuser_TextChanged(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(txtuser.Text) == true)
+            {
+                lbluser.Show();
+            }
+        }
+
+        private void txtpass_TextChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtpass.Text) == true)
+            {
+                lblpass.Show();
+            }
+        }
+
+        private void txtuser_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtuser.Focus();
+            lbluser.Hide();
+            if(txtuser.Focus() == true)
+            {
+               
+
+                if (String.IsNullOrEmpty(txtpass.Text) == true)
+                {
+                    lblpass.Show();
+                }
+            }
+        }
+
+        private void txtpass_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtpass.Focus();
+            lblpass.Hide();
+            if (txtpass.Focus() == true)
+            {
+                
+
+                if (String.IsNullOrEmpty(txtuser.Text) == true)
+                {
+                    lbluser.Show();
+                }
+            }
+        }
+    }
+}
